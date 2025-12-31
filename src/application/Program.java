8bcd88601b59788810f3ddfdf3399ae3cbe55f9e
Pedro Trindade <prindade@outlook.com>
2025-12-31 18:00:46 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -28,5 +29,10 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=====TEST 4: Seller insert=====");
+		Seller newSeller = new Seller(null, "Greg Green", "greg@gmail.com", new Date(), 4000.00, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New Id: = " + newSeller.getId());
 	}
 }
