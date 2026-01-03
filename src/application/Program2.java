d511e8b1f47678fad8c45289e4c06d55b32a891e
Pedro Trindade <prindade@outlook.com>
2026-01-02 23:18:13 -0300

    Department findAll implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -13,6 +15,12 @@ public class Program2 {
 		System.out.println("=====TEST 1: Department findById=====");
 		Department department = departmentDao.findById(3);
 		System.out.println(department);
+		
+		System.out.println("=====TEST 2: Department findAll=====");
+		List<Department> list = departmentDao.findAll();
+		for (Department dep : list) {
+			System.out.println(dep);
+		}
 	}
 
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
@@ -49,8 +50,8 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 			rs = st.executeQuery();
 			
 			if (rs.next()) {
-				Department obj = instantiateDepartment(rs);
-				return obj;
+				Department dep = instantiateDepartment(rs);
+				return dep;
 			}
 			return null;
 		} catch (SQLException e) {
@@ -71,7 +72,26 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 	@Override
 	public List<Department> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement("Select * from department");
+			
+			rs = st.executeQuery();
+			List<Department> list = new ArrayList<>();
+			while (rs.next()) {
+				Department dep = instantiateDepartment(rs);
+				list.add(dep);
+			}
+			return list;
+		} 
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 }
